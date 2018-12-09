8e43eccd8d3e36a6218108960242fb0a4dc76fa8
vidhurraj147 <vidhurraj147@gmail.com>
2018-12-08 20:19:55 -0500

    added some statements

--- src/app/date/date.component.ts
+++ src/app/date/date.component.ts
@@ -10,14 +10,26 @@ export class DateComponent implements OnInit {
 
   // message: string = new Date().toDateString(); 
   dateMessage: string;
+  someDate: string;
   
 
   constructor() { 
+    setInterval(() =>{
     let currentDate = new Date();
-    this.dateMessage = currentDate.toDateString() + ' ' + currentDate.toLocaleTimeString();
+    this.dateMessage = currentDate.toDateString() + ' ' + currentDate.toLocaleTimeString();    
+  },1000);
+
+
   }
 
+  // someDate = new Date();
+
+  // demoDate(){
+  //   this.someDate = new Date().toDateString();
+  // }
+
   ngOnInit() {
+    
   }
 
 }
